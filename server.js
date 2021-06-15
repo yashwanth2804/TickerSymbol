3fec6df4470691deb69522552555c9cb1e9b039b
Ben Gallagher <b8009285@newcastle.ac.uk>
2021-06-15 16:58:48 +0100

    Added an extension route to allow for limiting of returned ticker information

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const Ticker = require('./data')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3005
 const app = express()
 
 
@@ -14,8 +14,41 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/keyword/:search/limit/:max', (req, res) => {
+    console.log("--------/keyword-----------"+req.params.search+"---limit:"+req.params.max)
+    let search = req.params.search;
+    let maxResults = Number.parseInt(req.params.max)
+    console.log(maxResults)
+    if(!isNaN(maxResults)){
+        search = search.toLowerCase();
+        res.setHeader('Content-Type', 'application/json');
+        const g  =Ticker.TickerSymbols.filter( f => 
+            (JSON.stringify(f).toLowerCase().indexOf(search) !== -1)
+        )
+
+
+        if(maxResults > g.length || maxResults == '0'){  
+            maxResults = g.length
+        }
+
+
+        let count = 0;
+        limited = []
+        while (count < maxResults && count < g.length){
+            limited.push(g[count])
+            count++
+        }
+
+        res.send(limited)
+    }else{
+        console.log("Limit must be a number")
+        res.send("Limit must be a number")
+    }
+    res.end()
+})
+
 app.get('/keyword/:search', (req, res) => {
-    console.log("--------/keyword-----------"+req.params.s)
+    console.log("--------/keyword-----------"+req.params.search)
     let search = req.params.search;
     search = search.toLowerCase();
     res.setHeader('Content-Type', 'application/json');
